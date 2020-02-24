6f2e10a342162c29d47eb77834a4c146360c2b3b
user <nono647@gmail.com>
2020-02-24 15:32:38 +0100

    your commit2

---
+++ src/config/firebase_config.js
@@ -0,0 +1,10 @@
+var firebaseConfig={
+  "apiKey": " AIzaSyBBRKhWosBz7DlPI9grw7CgPXI4ET2EmCk ",
+  "authDomain": "app-creat1.firebaseapp.com",
+  "databaseURL": "https://app-creat1.firebaseio.com",
+  "projectId": "app-creat1",
+  "storageBucket": "app-creat1.appspot.com",
+  "messagingSenderId": "1056834174515",
+  "appId": "1:1056834174515:web:0126616e81167ed021ad46"
+};
+exports.config=firebaseConfig;
